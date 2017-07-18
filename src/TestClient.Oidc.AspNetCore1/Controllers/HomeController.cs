768080a61581d4647ff667cebec5637a902317ac
Scott Brady <scott.brady@rocksolidknowledge.com>
2017-07-18 19:19:03 +0100

    Added oidc client using ASP.NET Core 1.x

---
+++ src/TestClient.Oidc.AspNetCore1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestClient.Oidc.AspNetCore1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Login()
+        {
+            return Redirect("Index");
+        }
+
+        public async Task<ActionResult> Logout()
+        {
+            await Request.HttpContext.Authentication.SignOutAsync("OpenIdConnect");
+            return Redirect("Index");
+        }
+    }
+}
---
+++ src/TestClient.Oidc.AspNetCore1/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TestClient.Oidc.AspNetCore1
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+            app.UseDeveloperExceptionPage();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {AuthenticationScheme = "cookie"});
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                Authority = "http://localhost:3000",
+                ClientId = "test_implicit_app",
+                ResponseType = "id_token",
+                Scope = {"openid", "profile"},
+                RequireHttpsMetadata = false,
+                SignInScheme = "cookie"
+            });
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
